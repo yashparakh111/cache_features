bf4bcdd80b5f19627fc0b3f2417be7a95e744b57
Yash Parakh <yash.parakh111@gmail.com>
2019-01-22 23:46:08 -0600

    Analyzes PC history

--- features_analysis/analysis/PC_history.py
+++ features_analysis/analysis/PC_history.py
@@ -1,25 +1,32 @@
 # this program prints PC history
 
 import sys
+import copy
+import os
 
-f = open(sys.argv[1])
 
-history = [None] * 5
-history_list = set()
-i = 0
-count = 0
+for program in os.listdir(sys.argv[1]):
+    history = [None] * 5
+    history_list_ordered = set()
+    history_list_unordered = set()
+    i = 0
+    count = 0
 
-for line in f:
-    history[i] = (line.split(" "))[1]
-    history_list.add(tuple(history))
+    f = open(os.path.join(sys.argv[1], program), "r")
+    for line in f:
+        history[i] = (line.split(" "))[1]
+        history_list_ordered.add(tuple(history))
 
-    history_temp = history
-    history_temp.sort()
+        history_temp = copy.copy(history)   # performs a shallow copy
+        history_temp.sort()
+        history_list_unordered.add(tuple(history_temp))
 
-    i = (i + 1) % 5
-    count = count + 1
+        i = (i + 1) % 5
+        count = count + 1
 
-print "Unique PC History Ratio (considering ordering): ", len(history_list), "/", count, "=", float(len(history_list)) / count
-
-print "Unique PC History Ratio (NOT considering ordering): ", 
+    
+    print "Program Name: ", program
+    print "Unique PC History Ratio (ordered): ", len(history_list_ordered), "/", count, "=", float(len(history_list_ordered)) / count
+    print "Unique PC History Ratio (unordered): ", len(history_list_unordered), "/", count, "=", float(len(history_list_unordered)) / count
+    print "--------------------------------------"
 f.close()
