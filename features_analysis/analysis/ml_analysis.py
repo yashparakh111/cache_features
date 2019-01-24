e8f4a415dcd7498a5808603a0d1cc6f597cb686d
Yash Parakh <yash.parakh111@gmail.com>
2019-01-23 21:37:07 -0600

    Fixed mistake where instead of analyzing PCs, I was analyzing read addresses

--- features_analysis/analysis/ml_analysis.py
+++ features_analysis/analysis/ml_analysis.py
@@ -1,5 +1,6 @@
 # Helpful links:
     # Counting Frequencies: https://stackoverflow.com/questions/2600191/how-can-i-count-the-occurrences-of-a-list-item
+    # Printing Data in tabular format: https://stackoverflow.com/questions/9535954/printing-lists-as-tabular-data
 
 import sys      # provides access to command-line arguments
 import os       # used to access all files under given directory
@@ -7,7 +8,9 @@ import time
 import numpy as np
 from collections import Counter
 import matplotlib.pyplot as plt
+from prettytable import PrettyTable
 
+plot_num = 1
 
 for program in os.listdir(sys.argv[1]):
     start = time.time()
@@ -16,8 +19,7 @@ for program in os.listdir(sys.argv[1]):
     # find Hawkeye predictor accuracy
     total_pred = 0
     num_pred_correct = 0
-    unique_PCs = set()
-    PC_list = []
+    unique_PCs = {}     # create an empty dict
 
     for line in f:
         read_instr = line.split(" ")
@@ -25,27 +27,32 @@ for program in os.listdir(sys.argv[1]):
         total_pred = total_pred + 1
         num_pred_correct = num_pred_correct + int(read_instr[-1])
         PC = int(read_instr[1], 16)
+
         if PC not in unique_PCs:
-            unique_PCs.add(PC:0)
+            unique_PCs[PC] = 0
         else:
+            unique_PCs[PC] = unique_PCs[PC] + 1
 
-        PC_list.append(PC)
+        #PC_list.append(PC)
 
-    PC_freq = np.bincount(PC_list)
+    #PC_freq = np.bincount(PC_list)
 
     #print unique_PCs[0], unique_PCs[1000], unique_PCs[100000], unique_PCs[-1000], unique_PCs[-1]
-    print "Program Name: \t\t", program
-    print "# Predicted Correctly: \t", num_pred_correct
-    print "Total #: \t\t", total_pred
-    print "Accuracy: \t\t", float(num_pred_correct)/total_pred
-    print "Dimensionality: \t", len(unique_PCs)
-    print "Unique PC Ratio: \t", float(len(unique_PCs))/total_pred*100, "%"
+    data = PrettyTable(['Property', 'Value'])
+    data.add_row(["Program Name", program])
+    data.add_row(["# Predicted Correctly", num_pred_correct])
+    data.add_row(["Total PCs", total_pred])
+    data.add_row(["Hawkeye Accuracy", str(num_pred_correct) + " (" + str(round(float(num_pred_correct)/total_pred*100, 2)) + "%)"])
+    data.add_row(["Dimensionality", len(unique_PCs)])
+    data.add_row(["Unique PC Ratio", str(round(float(len(unique_PCs))/total_pred*100, 3)) + "%"])
+    print data
+    
+    print "Execute Time: ", time.time() - start, "ms\n"
 
-    print "-------------------------------------->", time.time() - start, "\n"
-
-    plt.subplot(3, 4, i)
+    plt.subplot(3, 4, plot_num)
     plt.title(program)
-    plt.plot(PC_freq)
+    plt.plot(unique_PCs.values())
+    plot_num = plot_num + 1
     f.close()
 
 plt.show()
