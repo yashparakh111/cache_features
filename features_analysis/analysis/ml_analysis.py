e2f86fc8fef5e73b42017c06f9951dd3581e3f1f
Yash Parakh <yash.parakh111@gmail.com>
2019-01-24 17:17:44 -0600

    Fixed mistakes

--- features_analysis/analysis/ml_analysis.py
+++ features_analysis/analysis/ml_analysis.py
@@ -26,7 +26,7 @@ for program in os.listdir(sys.argv[1]):
 
         total_pred = total_pred + 1
         num_pred_correct = num_pred_correct + int(read_instr[-1])
-        PC = int(read_instr[1], 16)
+        PC = int(read_instr[2], 16)
 
         if PC not in unique_PCs:
             unique_PCs[PC] = 0
@@ -40,13 +40,12 @@ for program in os.listdir(sys.argv[1]):
     #print unique_PCs[0], unique_PCs[1000], unique_PCs[100000], unique_PCs[-1000], unique_PCs[-1]
     data = PrettyTable(['Property', 'Value'])
     data.add_row(["Program Name", program])
-    data.add_row(["# Predicted Correctly", num_pred_correct])
     data.add_row(["Total PCs", total_pred])
     data.add_row(["Hawkeye Accuracy", str(num_pred_correct) + " (" + str(round(float(num_pred_correct)/total_pred*100, 2)) + "%)"])
     data.add_row(["Dimensionality", len(unique_PCs)])
     data.add_row(["Unique PC Ratio", str(round(float(len(unique_PCs))/total_pred*100, 3)) + "%"])
     print data
-    
+
     print "Execute Time: ", time.time() - start, "ms\n"
 
     plt.subplot(3, 4, plot_num)
