e02395a64fa66712833213595c0f655d5ddb37ea
Yash Parakh <yash.parakh111@gmail.com>
2019-01-24 00:35:11 -0600

    Add coded to look at multiple PC-History lengths

--- features_analysis/analysis/PC_history.py
+++ features_analysis/analysis/PC_history.py
@@ -1,43 +1,50 @@
 # this program analyzes PC history
 
 import sys
-import copy
 import os
+from collections import deque
 from prettytable import PrettyTable
+import time
 
 pc_history_len_list = [2, 4, 8, 16]
 
-data = PrettyTable(['Property', 'Value'])
+history_list_ordered = set()
+history_list_unordered = set()
+
+data = PrettyTable()
+
 for program in os.listdir(sys.argv[1]):
-    data.add_row(["Program Name", program])
+    start = time.time()
+    data.add_column("Unique PC-History Ratio", ["Ordered", "Unordered"])
 
     for pc_history_index in range(len(pc_history_len_list)):
         pc_history_len = pc_history_len_list[pc_history_index]
 
-        history = [None] * pc_history_len
-        history_list_ordered = set()
-        history_list_unordered = set()
-        i = 0
+        history = deque(maxlen=pc_history_len)
+        history_list_ordered.clear()
+        history_list_unordered.clear()
         count = 0
 
         f = open(os.path.join(sys.argv[1], program), "r")
 
         for line in f:
-            history[i] = (line.split(" "))[1]
+            history.append((line.split(" "))[1])
             history_list_ordered.add(tuple(history))
 
-            history_temp = copy.copy(history)   # performs a shallow copy
+            history_temp = list(history)
             history_temp.sort()
             history_list_unordered.add(tuple(history_temp))
 
-            i = (i + 1) % pc_history_len
             count = count + 1
 
-        data.add_row(["Total PC History Count", count])
-        data.add_row(["Unique PC History Ratio (ordered):" + str(pc_history_len), float(len(history_list_ordered))/count)])
-        data.add_row(["Unique PC History Ratio (unordered):" + str(pc_history_len), float(len(history_list_unordered))/count])
+        data.add_column(str(pc_history_len), [float(len(history_list_ordered))/count, float(len(history_list_unordered))/count])
 
-    f.close()
+        f.close()
 
-    data.clear_rows()
+    print "Program Name:", program
+    print "PC-History Count:", count
+    print data
+    print "Execute Time:", time.time() - start
+    data.clear()
+    
     print
